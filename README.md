3429b1d4b6ef4c033b6d82a5bdc302a82da40c9f
Adam Dobrawy <naczelnik@jawnosc.tk>
2017-02-04 02:07:14 +0100

    Refactor to support CSV and CLI output

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ Install python dependencies: ``pip install -r requirements.txt``
 ## Usage
 
     usage: trello-google-audit.py [-h] --key KEY [--sheet SHEET]
-                                  [--organization ORGANIZATION]
+                                  [--organization ORGANIZATION] [--csv CSV]
 
     optional arguments:
       -h, --help            show this help message and exit
@@ -23,3 +23,34 @@ Install python dependencies: ``pip install -r requirements.txt``
       --sheet SHEET         A Google sheet no. of published document (default: 1)
       --organization ORGANIZATION
                             Trello organization name (default: koonaukowegenbit)
+      --csv CSV
+
+## Example
+
+Generate tabular report user membership to boards, organization and sheet
+
+``
+$ python trello-google-audit.py --key '1-RfXe7NNukxxxxn-xxxxx_Zop-KfNZKaXeFPsg' --csv 'report.csv'; 
+``
+In effects CSV file with files with content like:
+
+| name                          | plotron | miklaj | ka54mil | tdarkness |
+|-------------------------------|---------|--------|---------|-----------|
+| NN - ogólna                   | True    | True   | True    | True      |
+| google                        | True    | True   | True    | True      |
+| trello                        | True    | True   | True    | True      |
+| U2F-Token                     | False   | True   | False   | False     |
+| Seminaria naukowe i warsztaty | True    | True   | False   | False     |
+
+Generate text report for missing membership to boards, organization and sheet:
+
+``
+$  python trello-google-audit.py --key '1-RfXexxxxxx-wTKSehCNi_Zop-KfNZKaXeFPsg'
+Google: plotron, tdarkness, mkczyk, miklaj
+Extra members of board NN - ogólna to organization: (None)
+Extra members of board Seminaria naukowe i warsztaty to organization: (None)
+Extra members of board U2F-Token to organization: (None)
+Extra members of Google to Trello organization: patkamil
+Extra members of Trello organization to Google: (None)
+
+``
--- trello-google-audit.py
+++ trello-google-audit.py
@@ -1,14 +1,15 @@
 #!/bin/python2.7
 # -*- coding: utf-8 -*-
-from __future__ import print_function
-from __future__ import unicode_literals
+from __future__ import print_function, unicode_literals
 
 import argparse
 import logging
-import os
 
+import requests
+from cached_property import cached_property
 from repoze.lru import lru_cache
 from trello import TrelloApi
+from unicodecsv import DictWriter
 
 try:
     TRELLO_APP_KEY = open('.app_key.txt').read().strip()
@@ -24,35 +25,139 @@ GOOGLE_KEY_FILE = '.key.txt'
 GOOGLE_SHEET_FILE = '.sheet.txt'
 
 
-def get_session():
-    if 'CACHE_REQUESTS' in os.environ:
-        import requests_cache
-        return requests_cache.CachedSession()
-    import requests
-    return requests.Session()
+class TrelloOrganization(object):
+    def __init__(self, api, organization):
+        self.api = api
+        self.organization = organization
+
+    @classmethod
+    def init(cls, organization):
+        trello = TrelloApi(TRELLO_APP_KEY)
+        try:
+            trello.set_token(open('.token.txt').read().strip())
+            logger.debug("Trello token loaded")
+        except IOError:
+            token_url = trello.get_token_url('Trello ', expires='never', write_access=True)
+            print("Enter following URL in your browser:", token_url)
+            token = raw_input("Enter token please:")
+            open('.token.txt', 'w').write(token)
+            trello.set_token(token)
+        return cls(trello, organization)
+
+    @cached_property
+    def organization_members(self):
+        return {x['username'] for x in self.api.organizations.get_member(self.organization)}
+
+    def _get_board_member(self, board):
+        return {self.get_member_name(member['idMember']) for member in board['memberships']}
+
+    @cached_property
+    def boards(self):
+        return self.api.organizations.get_board(self.organization)
+
+    @lru_cache(maxsize=64)
+    def get_member_name(self, idMember):
+        return self.api.members.get(idMember)['username']
+
+    @cached_property
+    def board_members(self):
+        return {board['name']: self._get_board_member(board) for board in self.boards}
 
 
-def trello_init():
-    trello = TrelloApi(TRELLO_APP_KEY)
-    try:
-        trello.set_token(open('.token.txt').read().strip())
-        logger.debug("Trello token loaded")
-    except IOError:
-        token_url = trello.get_token_url('Trello ', expires='never', write_access=True)
-        print("Enter following URL in your browser:", token_url)
-        token = raw_input("Enter token please:")
-        open('.token.txt', 'w').write(token)
-        trello.set_token(token)
-    return trello
+class Google(object):
+    def __init__(self, key, sheet):
+        self.key = key
+        self.sheet = sheet
+        self.session = requests.Session()
+        self.content = self._fetch()
+
+    @cached_property
+    def members(self):
+        return {x['gsx$trello']['$t']
+                for x in self.content['feed']['entry']
+                if x['gsx$trello']['$t']}
+
+    def _fetch(self):
+        url = "https://spreadsheets.google.com/feeds/list/%s/%d/public/values?alt=json" % (
+            self.key, self.sheet)
+        response = self.session.get(url)
+        return response.json()
 
 
-def google_fetch(key, sheet=1):
-    url = "https://spreadsheets.google.com/feeds/list/%s/%d/public/values?alt=json" % (
-        key, sheet)
-    return get_session().get(url).json()
+class Auditor(object):
+
+    def __init__(self, trello, google):
+        self.google = google
+        self.trello = trello
+
+    def extra_boards_members(self):
+        return {board: (members - self.trello.organization_members)
+                for board, members in self.trello.board_members.items()}.items()
+
+    def extra_organization_members(self):
+        org_m = self.trello.organization_members
+        google_m = self.google.members
+        return (org_m - google_m), (google_m - org_m)
+
+    def get_members(self):
+        trello_members = self.trello.organization_members
+        google_members = self.google.members
+        board_members = self.trello.board_members
+        return trello_members, google_members, board_members
 
 
-def build_args():
+class CLI(object):
+    def __init__(self, auditor):
+        self.auditor = auditor
+
+    def get_members(self):
+        print("Google:", ", ".join(self.auditor.google.members))
+
+    def print_extra_boards_members(self):
+        for board, extra_members in self.auditor.extra_boards_members():
+            print("Extra members of board {board} to organization:".format(board=board),
+                  ", ".join(extra_members) or '(None)')
+
+    def print_extra_members(self):
+        to_trello, to_google = self.auditor.extra_organization_members()
+
+        print("Extra members of Google to Trello organization:",
+              ", ".join(to_trello) or '(None)')
+        print("Extra members of Trello organization to Google:",
+              ", ".join(to_google) or '(None)')
+
+    def report(self):
+        self.get_members()
+        self.print_extra_boards_members()
+        self.print_extra_members()
+
+
+class CSV(object):
+    def __init__(self, auditor, filename='report.csv'):
+        self.auditor = auditor
+        self.filename = filename
+
+    def report(self):
+        trello_members, google_members, board_members = self.auditor.get_members()
+        all_members = set()
+        all_members.update(trello_members)
+        all_members.update(google_members)
+        for members in board_members.values():
+            all_members.update(members)
+
+        with open(self.filename, 'wb+') as fp:
+            csv = DictWriter(fp, ['name'] + list(all_members))
+            csv.writeheader()
+            board_members['google'] = google_members
+            board_members['trello'] = trello_members
+
+            for board, members in board_members.items():
+                row = {member: (member in members) for member in all_members}
+                row['name'] = board
+                csv.writerow(row)
+
+
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--key",
                         help="A Google sheet key of published document",
@@ -64,51 +169,16 @@ def build_args():
                         type=int)
     parser.add_argument("--organization",
                         help="Trello organization name (default: koonaukowegenbit)",
-                        default='koonaukowegenbit')
-    return parser.parse_args()
+                        default='uphnn')
+    parser.add_argument("--csv", type=str, default=None)
 
-
-@lru_cache(maxsize=64)
-def get_member_name(trello, idMember):
-    return trello.members.get(idMember)['username']
-
-
-def get_google_members(google):
-    return {x['gsx$trello']['$t'] for x in google['feed']['entry'] if x['gsx$trello']['$t']}
-
-
-def get_organization_members(trello, name):
-    return {x['username'] for x in trello.organizations.get_member(name)}
-
-
-def get_board_member(trello, board):
-    return {get_member_name(trello, member['idMember']) for member in board['memberships']}
-
-
-def print_extra_boards_members(trello, org_members, organization):
-    for board in trello.organizations.get_board('koonaukowegenbit'):
-        members = get_board_member(trello, board)
-        extra_members = members - org_members
-        print("Extra members of board {board} to organization:".format(board=board['name']),
-              ", ".join(extra_members) or '(None)')
-
-
-def print_extra_members(org_m, google_m):
-    print("Extra members of Google to Trello organization:",
-          ", ".join(org_m - google_m) or '(None)')
-    print("Extra members of Trello organization to Google:",
-          ", ".join(google_m - org_m) or '(None)')
-
-
-def main():
-    args = build_args()
-    trello = trello_init()
-    google = google_fetch(args.key, args.sheet)
-    organization_members = get_organization_members(trello, args.organization)
-    google_members = get_google_members(google)
-
-    print_extra_boards_members(trello, organization_members, args.organization)
-    print_extra_members(organization_members, google_members)
+    args = parser.parse_args()
+    auditor = Auditor(trello=TrelloOrganization.init(args.organization),
+                      google=Google(args.key, args.sheet))
+    if args.csv:
+        CSV(auditor, args.csv).report()
+    else:
+        CLI(auditor).report()
 
 
 if __name__ == '__main__':
